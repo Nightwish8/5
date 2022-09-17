a8c15da02c4869eb537a84443afb51ecf1a680aa
Nightwish8 <104369603+Nightwish8@users.noreply.github.com>
2022-09-17 18:36:29 +0300

    Update test4.js

--- test4.js
+++ test4.js
@@ -1,18 +1,37 @@
-// function showup() {
-//     document.getElementById('show').style.opacity = 1;
+// function change_style(){
+//     let a = document.getElementById('show')
+//     if (window.getComputedStyle(a).color == "red")
+//     {
+//         a.style.color = "green"
+//     }
+//     else
+//     a.style.color = "red"
 // }
-// setTimeout(showup,1000)
 
 
 
-
-
-function change_style(){
-    let a = document.getElementById('show')
-    if (window.getComputedStyle(a).color == "red")
+function change_style2(){
+    a = document.getElementById('show');
+    b = window.getComputedStyle(a).backgroundColor;
+    alert (b);
+    if ( b == "rgb(255,0,0)" )
     {
-        a.style.color = "green"
+        a.style.backgroundColor = "rgb(0,255,0)"
     }
     else
-    a.style.color = "red"
+        a.style.backgroundColor = "rgb(255,0,0)"
+}
+
+
+
+function change_style3(){
+    a = document.getElementById('show');
+    b = window.getComputedStyle(a).padding;
+    alert (b);
+    if ( b != "20px" )
+    {
+        a.style.padding = "20px"
+    }
+    else
+        a.style.padding = "10px"
 }
